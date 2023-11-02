906fe46f9b4cdac1e52988ad53c8b891494ce82e
Zana-hex <zanacr2204@gmail.com>
2023-11-01 23:35:33 -0600

    Añadimos verificaciones y demás asignaciones

--- src/main/java/Controlador/Controlador.java
+++ src/main/java/Controlador/Controlador.java
@@ -11,6 +11,10 @@ import java.util.Queue;
 public class Controlador {
 
     int memoriaDisponible = 1024;
+    int impresorasDisponibles = 2;
+    int escanersDisponibles = 1;
+    int modemsDisponibles = 1;
+    int cdsDisponibles = 2;
     final int bloqueMemoria = 32;
 
     int contadorId = 0;
@@ -22,45 +26,102 @@ public class Controlador {
     Queue<Proceso> prioridad3 = new LinkedList<>();
 
     public Controlador() {
-
+        Proceso proceso = colaProcesos.poll();
     }
 
-    public void asignacion() throws InterruptedException {
-        boolean bandera = true;
-        int multiplicador = 2;
+    public void asignacion(Proceso actual) throws InterruptedException {
         while (!colaProcesos.isEmpty()) {
-            Proceso actual = colaProcesos.poll();
+            boolean bandera = true;
+            int multiplicador = 2;
+
             if (actual.getPrioridad() == 0) {
                 if (memoriaDisponible - 64 >= 0) {
                     actual.setMemoriaAsignada(64);
                     memoriaDisponible -= 64;
                     colaTiempoReal.offer(actual);
+                    Thread.sleep(1000);
+                } else {
+                    colaProcesos.offer(actual);
                 }
-                Thread.sleep(1000);
+
             }
 
-            if (actual.getPrioridad() == 1) {
+            if (actual.getPrioridad() == 1 || actual.getPrioridad() == 2 || actual.getPrioridad() == 3) {
                 if (actual.getMegas() <= bloqueMemoria) {
                     if (memoriaDisponible - bloqueMemoria >= 0) {
-                        actual.setMemoriaAsignada(bloqueMemoria);
-                        memoriaDisponible -= bloqueMemoria;
-                        prioridad1.offer(actual);
-                    } else {
-
+                        if (verificarCds(actual) && validacionModems(actual) && validacionEscaners(actual)
+                                && validacionImpresoras(actual))
+                        {
+                            actual.setMemoriaAsignada(bloqueMemoria);
+                            memoriaDisponible -= bloqueMemoria;
+                            if (actual.getPrioridad() == 1) {
+                                prioridad1.offer(actual);
+                            }
+                            if (actual.getPrioridad() == 2) {
+                                prioridad2.offer(actual);
+                            }
+                            if (actual.getPrioridad() == 3) {
+                                prioridad3.offer(actual);
+                            }
+                        } else {
+                            colaProcesos.offer(actual);
+                        }
                     }
                 } else {
                     while (bandera) {
-
-                        if (actual.getMegas() <= 32*multiplicador){
-                            actual.setMemoriaAsignada(32*multiplicador);
-
+                        if (actual.getMegas() <= bloqueMemoria * multiplicador) {
+                            if (memoriaDisponible - bloqueMemoria * multiplicador >= 0) {
+                                if (verificarCds(actual) && validacionModems(actual) && validacionEscaners(actual)
+                                        && validacionImpresoras(actual)) {
+                                    actual.setMemoriaAsignada(bloqueMemoria * multiplicador);
+                                    if (actual.getPrioridad() == 1) {
+                                        prioridad1.offer(actual);
+                                    }
+                                    if (actual.getPrioridad() == 2) {
+                                        prioridad2.offer(actual);
+                                    }
+                                    if (actual.getPrioridad() == 3) {
+                                        prioridad3.offer(actual);
+                                    }
+                                } else {
+                                    colaProcesos.offer(actual);
+                                }
+                            }
+                            bandera = false;
+                        } else {
+                            multiplicador++;
                         }
                     }
                 }
                 Thread.sleep(1000);
             }
+        }
+    }
 
+    public boolean validacionImpresoras(Proceso actual) {
+        if (actual.getNumImpresoras() <= impresorasDisponibles) {
+            impresorasDisponibles -= actual.getNumImpresoras();
+            return true;
+        } else {
+            return false;
+        }
+    }
 
+    public boolean validacionModems(Proceso actual) {
+        if (actual.getNumModems() <= modemsDisponibles) {
+            modemsDisponibles -= actual.getNumModems();
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean validacionEscaners(Proceso actual) {
+        if (actual.getNumEscaners() <= escanersDisponibles) {
+            escanersDisponibles -= actual.getNumEscaners();
+            return true;
+        } else {
+            return false;
         }
     }
 
